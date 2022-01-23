69b326dda9417be23f89577dec4b5ecbff5ca499
lhasan <haesan@gmail.com>
2022-01-24 01:19:47 +0900

    Update Theme.js

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -3,7 +3,7 @@ const BORDER_RADIUS = "4px";
 
 export default {
   maxWidth: "935px",
-  bgColor: "#FAFA",
+  bgColor: "#FAFAFA",
   blackColor: "#262626",
   darkGreyColor: "#999",
   lightGreyColor: "#c7c7c7",
